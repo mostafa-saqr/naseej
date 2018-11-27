feb291e0649e5e954e24416744f290cf38f7ac92
Mostafa <mostafa@enhance.online>
2018-11-27 16:17:57 +0200

    nassej prject

---
+++ src/js/nassej.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+    let servicsObj = [
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        },
+        {
+            title:'نص لوريم إيبسوم القياسي والمستخدم',
+            description:'لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)ويستخدم في صناعات المطابع ودور النشر. كان لوريم ايبسوم ولازال'
+        }
+    ]
+    $(".header--slider").owlCarousel({
+        items:1,
+        rtl:true
+    });
+    function runServicesCarusel(){
+        $(".service-items").owlCarousel({
+            items:4,
+            rtl:true,
+            loop:false,
+            margin:20,
+            dots:false,
+            nav:true,
+            responsive : {
+                // breakpoint from 0 up
+                0 : {
+                    items:1,
+                    nav:false,
+                    dots:true
+                },
+                // breakpoint from 480 up
+                480 : {
+                    items:1,
+                    nav:false,
+                    dots:true
+                },
+                // breakpoint from 768 up
+                768 : {
+                    items:3,
+                    nav:false,
+                    dots:true
+                },
+                // breakpoint from 1200 up
+                1200 : {
+                    items:4
+                }
+            }
+        });
+    }
+    function callIndvedualServices(){
+        servicsObj.map(
+           function(item){
+            $('#indvedualServices').append(`
+            <div class="item">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.title}</h5>
+                        <p class="card-text">${item.description}</p>
+                    </div>
+                    <div class="card-footer">
+                        <ul class="list-inline p-0 m-0">
+                            <li class="list-inline-item">
+                                <img src="./src/images/icons/n-img_24.png" alt="">
+                            </li>
+                            <li class="list-inline-item">
+                                <img src="./src/images/icons/n-img_26.png" alt="">
+                            </li>
+                            <li class="list-inline-item float-left">
+                                <a href="#/">استفد من الخدمة</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        `)
+           }
+        )
+        
+        runServicesCarusel()
+    }
+    callIndvedualServices();
+});
